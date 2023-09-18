18522db56b12c65d72594886aec385179e840325
Marcus Adebayo <marcus.adebayo8@gmail.com>
2023-09-18 12:28:53 -0500

    config

--- config.py
+++ config.py
@@ -6,9 +6,33 @@ import json
 # Load environment variables from .env file
 load_dotenv()
 
-# Load credentials
-with open('credentials.json', 'r') as f:
-    creds_data = json.load(f)
+# List of expected keys for the credentials
+keys = [
+    "type",
+    "project_id",
+    "private_key_id",
+    "private_key",
+    "client_email",
+    "client_id",
+    "auth_uri",
+    "token_uri",
+    "auth_provider_x509_cert_url",
+    "client_x509_cert_url",
+    "universe_domain"
+]
+
+# Initialize an empty creds_data dictionary
+creds_data = {}
+
+# Check if credentials.json exists
+if os.path.exists('credentials.json'):
+    # Load credentials.json
+    with open('credentials.json', 'r') as f:
+        creds_data = json.load(f)
+else:
+    # Fetch credentials from environment variables if credentials.json does not exist
+    for key in keys:
+        creds_data[key] = os.getenv(f'CRED_{key}')
 
 SUBJECT_EMAIL = os.environ.get('SUBJECT_EMAIL')
 if not SUBJECT_EMAIL:
